72c3087642c8ac8ec1f73c1700e28ce1810a2904
PedroBittarBarao <pedrobaraocontas@gmail.com>
2025-06-03 16:19:48 -0300

    no amount cancelation functionality

---
+++ lib/CancelTransaction.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module CancelTransaction (cancelTransaction) where
+
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.Aeson as Aeson
+import Data.Text (Text)
+
+cancelTransaction :: Text -> IO ()
+cancelTransaction txId = do
+  manager <- newManager tlsManagerSettings
+  let url = "http://localhost:5001/cancelar"
+      payload = Aeson.object ["transaction_id" Aeson..= txId]
+      requestBody = RequestBodyLBS (Aeson.encode payload)
+  initReq <- parseRequest url
+  let request = initReq
+        { method = "POST"
+        , requestBody = requestBody
+        , requestHeaders = [("Content-Type", "application/json")]
+        }
+  response <- httpLbs request manager
+  putStrLn $ "❌ Enviada cancelamento: " ++ show (responseStatus response)
--- lib/RequestHandler.hs
+++ lib/RequestHandler.hs
@@ -2,54 +2,43 @@
 
 module RequestHandler (app) where
 
-import Network.Wai (Application, Request, Response, responseLBS, strictRequestBody)
+import Network.Wai (Application, Request, Response, responseLBS, requestMethod, strictRequestBody, rawPathInfo)
 import Network.HTTP.Types (status200, status400)
 import Data.Aeson (decode, encode, object, (.=))
 import qualified Data.ByteString.Lazy.Char8 as BL
-import Data.Text (Text)
-import Data.IORef
 import qualified Data.Set as Set
-import Control.Exception (try, SomeException)
-
+import Data.IORef
+import Data.Text (Text)
 import WebhookEvent (WebhookEvent(..))
 import ConfirmTransaction (confirmTransaction)
+import CancelTransaction (cancelTransaction)
 
 app :: IORef (Set.Set Text) -> Application
 app processedRef req respond = do
-  body <- strictRequestBody req
-  putStrLn "Received raw body:"
-  BL.putStrLn body
+  let path = rawPathInfo req
+  if path == "/webhook"
+    then do
+      body <- strictRequestBody req
+      putStrLn "Received raw body:"
+      BL.putStrLn body
 
-  case decode body :: Maybe WebhookEvent of
-    Just event -> do
-      let txId = transaction_id event
-      alreadyProcessed <- Set.member txId <$> readIORef processedRef
-
-      if alreadyProcessed
-        then do
-          putStrLn $ "Transação já processada: " ++ show txId
-          respond $ responseLBS
-            status400
-            [("Content-Type", "application/json")]
-            (encode $ object ["error" .= ("duplicated transaction" :: Text)])
-        else do
+      case decode body :: Maybe WebhookEvent of
+        Just event -> do
           putStrLn "Parsed event:"
           print event
-          modifyIORef' processedRef (Set.insert txId)
-
-          result <- try (confirmTransaction txId) :: IO (Either SomeException ())
-          case result of
-            Left err -> putStrLn $ "Erro ao confirmar transação: " ++ show err
-            Right _  -> putStrLn "Confirmação enviada com sucesso"
-
-          respond $ responseLBS
-            status200
-            [("Content-Type", "application/json")]
-            (encode $ object ["transaction_id" .= txId])
-
-    Nothing -> do
-      putStrLn "Failed to parse WebhookEvent"
-      respond $ responseLBS
-        status400
-        [("Content-Type", "application/json")]
-        (encode $ object ["error" .= ("invalid payload" :: Text)])
+          alreadyProcessed <- Set.member (transaction_id event) <$> readIORef processedRef
+          if alreadyProcessed
+            then do
+              putStrLn "Duplicated transaction"
+              respond $ responseLBS status400 [("Content-Type", "application/json")] (encode $ object ["error" .= ("duplicated transaction" :: Text)])
+            else do
+              modifyIORef processedRef (Set.insert (transaction_id event))
+              if amount event == 0.00
+                then cancelTransaction (transaction_id event)
+                else confirmTransaction (transaction_id event)
+              respond $ responseLBS status200 [("Content-Type", "application/json")] (encode $ object ["transaction_id" .= transaction_id event])
+        Nothing -> do
+          putStrLn "Failed to parse WebhookEvent"
+          respond $ responseLBS status400 [("Content-Type", "application/json")] (encode $ object ["error" .= ("invalid payload" :: Text)])
+    else
+      respond $ responseLBS status400 [("Content-Type", "application/json")] (encode $ object ["error" .= ("invalid route" :: Text)])
