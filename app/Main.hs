b5ab8d2a9a129a4218e26dd59f157bb4d7426449
PedroBittarBarao <pedrobaraocontas@gmail.com>
2025-06-03 16:16:08 -0300

    duplicate id functionality

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,14 @@
+import Data.IORef
+import qualified Data.Set as Set
+import Data.Text (Text)
 import Network.Wai.Handler.Warp (run)
 
+
 import WebhookEvent
-import RequestHandler
+import RequestHandler (app)
 
 main :: IO ()
 main = do
   putStrLn "Listening on port 5001..."
-  run 5000 app
-
+  processedRef <- newIORef Set.empty
+  run 5000 (app processedRef)
--- lib/RequestHandler.hs
+++ lib/RequestHandler.hs
@@ -1,42 +1,55 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module RequestHandler (app) where
 
 import Network.Wai (Application, Request, Response, responseLBS, strictRequestBody)
-import Network.HTTP.Types (status200)
+import Network.HTTP.Types (status200, status400)
 import Data.Aeson (decode, encode, object, (.=))
 import qualified Data.ByteString.Lazy.Char8 as BL
-import Control.Exception (try, SomeException)
 import Data.Text (Text)
+import Data.IORef
+import qualified Data.Set as Set
+import Control.Exception (try, SomeException)
 
 import WebhookEvent (WebhookEvent(..))
 import ConfirmTransaction (confirmTransaction)
 
-app :: Application
-app req respond = do
+app :: IORef (Set.Set Text) -> Application
+app processedRef req respond = do
   body <- strictRequestBody req
   putStrLn "Received raw body:"
   BL.putStrLn body
 
   case decode body :: Maybe WebhookEvent of
     Just event -> do
-      putStrLn "Parsed event:"
-      print event
       let txId = transaction_id event
-      
-      -- Tenta enviar a confirmação e captura exceções para não quebrar o servidor
-      result <- try (confirmTransaction txId) :: IO (Either SomeException ())
-      case result of
-        Left err -> putStrLn $ "Erro ao confirmar transação: " ++ show err
-        Right _  -> putStrLn "Confirmação enviada com sucesso"
+      alreadyProcessed <- Set.member txId <$> readIORef processedRef
 
-      respond $ responseLBS
-        status200
-        [("Content-Type", "application/json")]
-        (encode $ object ["transaction_id" .= txId])
+      if alreadyProcessed
+        then do
+          putStrLn $ "Transação já processada: " ++ show txId
+          respond $ responseLBS
+            status400
+            [("Content-Type", "application/json")]
+            (encode $ object ["error" .= ("duplicated transaction" :: Text)])
+        else do
+          putStrLn "Parsed event:"
+          print event
+          modifyIORef' processedRef (Set.insert txId)
+
+          result <- try (confirmTransaction txId) :: IO (Either SomeException ())
+          case result of
+            Left err -> putStrLn $ "Erro ao confirmar transação: " ++ show err
+            Right _  -> putStrLn "Confirmação enviada com sucesso"
+
+          respond $ responseLBS
+            status200
+            [("Content-Type", "application/json")]
+            (encode $ object ["transaction_id" .= txId])
 
     Nothing -> do
       putStrLn "Failed to parse WebhookEvent"
       respond $ responseLBS
-        status200
+        status400
         [("Content-Type", "application/json")]
         (encode $ object ["error" .= ("invalid payload" :: Text)])
